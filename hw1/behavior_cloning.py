2a3fc2640326e027a0dbce21bb92bef253d19f73
psktam <peter.kim427@gmail.com>
2017-09-03 09:05:41 -0700

    Got it working well, I think. The hopper is pretty bad, but it's an unstable control problem, so I think that's okay for the time being.

--- hw1/behavior_cloning.py
+++ hw1/behavior_cloning.py
@@ -1,10 +1,12 @@
 """Implements behavioral cloning, which is just supervised learning algorithm"""
+import argparse
+import pickle
+
 import tensorflow as tf
 import gym
 import numpy as np
 
 import load_policy
-import tf_util
 import models
 
 
@@ -13,73 +15,89 @@ def generate_samples(policy_fn, environment, num_samples):
     if num_samples > environment.spec.timestep_limit:
         raise ValueError("Asking for {} steps. Can draw at most {} samples".format(num_samples,
                                                                                    environment.spec.timestep_limit))
+    total_reward = 0.0
+    observations = []
+    actions = []
 
-    with tf.Session():
-        tf_util.initialize()
-        total_reward = 0.0
-        observations = []
-        actions = []
+    current_observation = environment.reset()
 
-        current_observation = environment.reset()
+    for _ in range(num_samples):
+        action = policy_fn(current_observation[None, :])
+        observations.append(current_observation)
+        actions.append(action)
 
-        for _ in range(num_samples):
-            action = policy_fn(current_observation[None, :])
-            observations.append(current_observation)
-            actions.append(action)
-
-            # Step into the next time
-            current_observation, reward, _, _ = environment.step(action)
-            total_reward += reward
+        # Step into the next time
+        current_observation, reward, _, _ = environment.step(action)
+        total_reward += reward
 
     return np.array(observations).squeeze(), np.array(actions).squeeze(), total_reward
 
 
-def train(loss_and_name_mapping, policy_function, environment,
-          training_epochs=10, expert_samples=1000, training_iters=1000, rtol=1e-6):
+def train(train_step, loss_function, name_mapping, policy_function, environment, session,
+          expert_samples=1000, training_iters=10000, rtol=1e-6):
     """
     Implements the training pipeline. The model is trained to try to match the given policy function, using the supplied
     openAI environment
     """
-    loss_function, name_mapping = loss_and_name_mapping
     # Generate expert observations and actions
     expert_obs, expert_actions, _ = generate_samples(policy_function, environment, expert_samples)
 
-    gradient_descent = tf.train.GradientDescentOptimizer(learning_rate=0.01)
     last_cost = np.inf
     costs = []
 
-    with tf.Session() as session:
-        tf.global_variables_initializer().run()
-        for training_iteration in range(training_iters):
-            # Select random batches
-            batch_samples = np.random.randint(0, expert_samples, 100)
-            batch_obs, batch_actions = expert_obs[batch_samples], expert_actions[batch_samples]
+    for training_iteration in range(training_iters):
+        # Select random batches
+        batch_size = int(0.1 * expert_samples)
+        batch_samples = np.random.randint(0, expert_samples, batch_size)
+        batch_obs, batch_actions = expert_obs[batch_samples], expert_actions[batch_samples]
 
-            session.run(gradient_descent.minimize(loss=loss_function),
-                        feed_dict={name_mapping['observations']: batch_obs, name_mapping['actions']: batch_actions})
-            cost = session.run(loss_function, feed_dict={name_mapping['observations']: expert_obs,
-                                                         name_mapping['actions']: expert_actions})
-            costs.append(cost)
-            relative_err = abs(cost - last_cost) / last_cost
+        session.run(train_step, feed_dict={name_mapping['observations']: batch_obs,
+                                           name_mapping['actions']: batch_actions})
+        cost = session.run(loss_function, feed_dict={name_mapping['observations']: expert_obs,
+                                                     name_mapping['actions']: expert_actions})
+        costs.append(cost)
+        relative_err = abs(cost - last_cost) / last_cost
 
-            if training_iteration % 10 == 0:
-                print("On training iteration {} out of {}".format(training_iteration, training_iters))
-                print("    Current cost is {}".format(cost))
-                print("    Relative error: {}".format(relative_err))
+        if training_iteration % 10 == 0:
+            print("On training iteration {} out of {}".format(training_iteration, training_iters))
+            print("    Current cost is {}".format(cost))
+            print("    Relative error: {}".format(relative_err))
 
-            last_cost = cost
-            if relative_err <= rtol:
-                break
+        last_cost = cost
+        if relative_err <= rtol:
+            break
     print("Finished training!")
 
 
 def main():
     """Some main logic"""
+    parser = argparse.ArgumentParser(description="Implement behavior cloning of the hopper expert policy")
+    parser.add_argument("model", type=str)
+    parser.add_argument("--training_rate", "-r", type=float, default=0.01)
+    parser.add_argument("--num_epochs", "-e", type=int, default=10)
+    parser.add_argument("--relative_err", "-x", type=float, default=1e-6)
+    args = parser.parse_args()
+
     expert_policy_file = 'experts/Hopper-v1.pkl'
-    my_model = models.MODEL_REGISTRY['cheating_hopper'](expert_policy_file)
+    my_model = models.MODEL_REGISTRY[args.model](expert_policy_file)
     env = gym.make("Hopper-v1")
     expert_policy = load_policy.load_policy(expert_policy_file)
-    train(my_model.loss_function, expert_policy, env)
+
+    training_epochs = args.num_epochs
+    loss_fun = my_model.loss_function
+    train_step = tf.train.GradientDescentOptimizer(learning_rate=args.training_rate).minimize(loss_fun)
+
+    train_step = tf.train.AdagradOptimizer(learning_rate=args.training_rate).minimize(loss_fun)
+
+    with tf.Session() as session:
+        tf.global_variables_initializer().run()
+        for epoch in range(training_epochs):
+            print("Epoch {} out of {}".format(epoch + 1, training_epochs))
+            train(train_step, loss_fun, my_model.varname_map, expert_policy, env, session, rtol=args.relative_err)
+
+        fname = input("Where do you want to save the derived policy? ")
+        with open(fname, 'wb') as savefile:
+            pickle.dump(my_model.save_variables(session), savefile)
 
 
 if __name__ == '__main__':
--- hw1/models/hopper.py
+++ hw1/models/hopper.py
@@ -10,28 +10,59 @@ class CheatingModel(ClonedModel):
     """cheating solution, wherein I just recreate the exact structure of the expert policy"""
     name = 'cheating_hopper'
 
-    @property
-    def loss_function(self):
-        """Produce the same structure"""
+    def __init__(self, pickle_file):
+        """Generate variables and map their names as well"""
+        super(self.__class__, self).__init__(pickle_file)
         hidden_dim = 64
 
         observations = tf.placeholder(dtype=np.float64, shape=(None, self._obs_dim), name='observations')
-        hidden_1 = tf.Variable(initial_value=np.random.random((self._obs_dim, hidden_dim)))
-        bias_1 = tf.Variable(initial_value=np.random.random((1, hidden_dim)))
-        hidden_2 = tf.Variable(initial_value=np.random.random((hidden_dim, hidden_dim)))
-        bias_2 = tf.Variable(initial_value=np.random.random((1, hidden_dim)))
-        output_layer = tf.Variable(initial_value=np.random.random((hidden_dim, self._action_dim)))
-        output_bias = tf.Variable(initial_value=np.random.random((1, self._action_dim)))
+        hidden_1 = tf.Variable(initial_value=np.random.random((self._obs_dim, hidden_dim)), name='hidden-1')
+        bias_1 = tf.Variable(initial_value=np.random.random((1, hidden_dim)), name='bias-1')
+        hidden_2 = tf.Variable(initial_value=np.random.random((hidden_dim, hidden_dim)), name='hidden-2')
+        bias_2 = tf.Variable(initial_value=np.random.random((1, hidden_dim)), name='bias-2')
+        output_layer = tf.Variable(initial_value=np.random.random((hidden_dim, self._action_dim)), name='output')
+        output_bias = tf.Variable(initial_value=np.random.random((1, self._action_dim)), name='output-bias')
+        actions = tf.placeholder(dtype=np.float64, shape=(None, self._action_dim), name='actions')
 
-        feed_forward = observations
-        for layer, bias in zip((hidden_1, hidden_2), (bias_1, bias_2)):
+        self.varname_map = {'observations': observations, 'actions': actions,
+                            'hidden-1': hidden_1, 'bias-1': bias_1,
+                            'hidden-2': hidden_2, 'bias-2': bias_2,
+                            'output': output_layer, 'output-bias': output_bias}
+
+    @property
+    def loss_function(self):
+        """Produce the same structure"""
+        feed_forward = (self.varname_map['observations'] - self._obs_mean) / self._obs_std
+        hidden_layers = [self.varname_map[key] for key in ('hidden-1', 'hidden-2')]
+        hidden_biases = [self.varname_map[key] for key in ('bias-1', 'bias-2')]
+        for layer, bias in zip(hidden_layers, hidden_biases):
             feed_forward = tf.tanh(tf.matmul(feed_forward, layer) + bias)
 
         # Do not apply nonlinearity to the output layer
-        feed_forward = tf.matmul(feed_forward, output_layer) + output_bias
+        feed_forward = tf.matmul(feed_forward, self.varname_map['output']) + self.varname_map['output-bias']
 
         # Hm, cost is interesting. Should this be a class-specific thing, or should that be something to decide when
         # optimizing? I'm going to go with the former for now.
-        actions = tf.placeholder(dtype=np.float64, shape=(None, self._action_dim), name='actions')
-        cost = tf.losses.absolute_difference(labels=actions, predictions=feed_forward)
-        return cost, {'observations': observations, 'actions': actions}
+
+        cost = tf.losses.absolute_difference(labels=self.varname_map['actions'], predictions=feed_forward)
+        return cost
+
+    def save_variables(self, session):
+        """Save the network in the same format as the encoded pickles"""
+        pickle_dict = {'nonlin_type': 'tanh'}
+        pickle_dict['GaussianPolicy'] = policy_dict = {}
+
+        policy_dict['out'] = {'AffineLayer': {'W': session.run(self.varname_map['output']),
+                                              'b': session.run(self.varname_map['output-bias'])}}
+        policy_dict['hidden'] = {'FeedforwardNet': {
+            'layer_0': {'AffineLayer': {
+                'W': session.run(self.varname_map['hidden-1']),
+                'b': session.run(self.varname_map['bias-1'])}},
+            'layer_2': {'AffineLayer': {
+                'W': session.run(self.varname_map['hidden-2']),
+                'b': session.run(self.varname_map['bias-2'])}}}}
+        meansq = self._obs_mean ** 2.0 + self._obs_std ** 2.0
+        policy_dict['obsnorm'] = {'Standardizer': {'mean_1_D': self._obs_mean, 'meansq_1_D': meansq}}
+        policy_dict['logstdevs_1_Da'] = None
+
+        return pickle_dict

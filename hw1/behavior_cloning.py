d152be475c71adf44557bcf4e4b76cc79dd5f56c
psktam <peter.kim427@gmail.com>
2017-09-12 12:52:50 -0700

    Got things working

--- hw1/behavior_cloning.py
+++ hw1/behavior_cloning.py
@@ -28,44 +28,43 @@ def generate_samples(policy_fn, environment, num_samples):
 
         # Step into the next time
         current_observation, reward, _, _ = environment.step(action)
+        # environment.render()
         total_reward += reward
 
     return np.array(observations).squeeze(), np.array(actions).squeeze(), total_reward
 
 
-def train(train_step, loss_function, name_mapping, policy_function, environment, session,
-          expert_samples=1000, training_iters=10000, rtol=1e-6):
+def train(train_step, loss_function, name_mapping, session, expert_obs, expert_actions, batch_size,
+          training_iters=1000000, rtol=1e-6):
     """
     Implements the training pipeline. The model is trained to try to match the given policy function, using the supplied
     openAI environment
     """
-    # Generate expert observations and actions
-    expert_obs, expert_actions, _ = generate_samples(policy_function, environment, expert_samples)
-
+    expert_samples = len(expert_obs)
     last_cost = np.inf
-    costs = []
 
-    for training_iteration in range(training_iters):
-        # Select random batches
-        batch_size = int(0.1 * expert_samples)
-        batch_samples = np.random.randint(0, expert_samples, batch_size)
-        batch_obs, batch_actions = expert_obs[batch_samples], expert_actions[batch_samples]
+    try:
+        for training_iteration in range(training_iters):
+            # Select random batches
+            batch_samples = np.random.randint(0, expert_samples, batch_size)
+            batch_obs, batch_actions = expert_obs[batch_samples], expert_actions[batch_samples]
 
-        session.run(train_step, feed_dict={name_mapping['observations']: batch_obs,
-                                           name_mapping['actions']: batch_actions})
-        cost = session.run(loss_function, feed_dict={name_mapping['observations']: expert_obs,
-                                                     name_mapping['actions']: expert_actions})
-        costs.append(cost)
-        relative_err = abs(cost - last_cost) / last_cost
+            session.run(train_step, feed_dict={name_mapping['observations']: batch_obs,
+                                               name_mapping['actions']: batch_actions})
+            cost = session.run(loss_function, feed_dict={name_mapping['observations']: batch_obs,
+                                                         name_mapping['actions']: batch_actions})
+            relative_err = abs(cost - last_cost) / last_cost
 
-        if training_iteration % 10 == 0:
-            print("On training iteration {} out of {}".format(training_iteration, training_iters))
-            print("    Current cost is {}".format(cost))
-            print("    Relative error: {}".format(relative_err))
+            if training_iteration % 10 == 0:
+                print("On training iteration {} out of {}".format(training_iteration, training_iters))
+                print("    Current cost is {}".format(cost))
+                print("    Relative error: {}".format(relative_err))
 
-        last_cost = cost
-        if relative_err <= rtol:
-            break
+            last_cost = cost
+            if relative_err <= rtol:
+                break
+    except KeyboardInterrupt:
+        pass
     print("Finished training!")
 
 
@@ -73,27 +72,38 @@ def main():
     """Some main logic"""
     parser = argparse.ArgumentParser(description="Implement behavior cloning of the hopper expert policy")
     parser.add_argument("model", type=str)
+    parser.add_argument("expert", type=str)
     parser.add_argument("--training_rate", "-r", type=float, default=0.01)
-    parser.add_argument("--num_epochs", "-e", type=int, default=10)
+    parser.add_argument("--num_epochs", "-e", type=int, default=20)
     parser.add_argument("--relative_err", "-x", type=float, default=1e-6)
+    parser.add_argument("--batch_size", "-b", type=int, default=100)
     args = parser.parse_args()
 
-    expert_policy_file = 'experts/Hopper-v1.pkl'
+    expert_policy_file = 'experts/{}-v1.pkl'.format(args.expert)
     my_model = models.MODEL_REGISTRY[args.model](expert_policy_file)
-    env = gym.make("Hopper-v1")
+    env = gym.make("{}-v1".format(args.expert))
     expert_policy = load_policy.load_policy(expert_policy_file)
 
     training_epochs = args.num_epochs
     loss_fun = my_model.loss_function
-    train_step = tf.train.GradientDescentOptimizer(learning_rate=args.training_rate).minimize(loss_fun)
-
     train_step = tf.train.AdagradOptimizer(learning_rate=args.training_rate).minimize(loss_fun)
 
+    all_obs, all_actions = [], []
+
     with tf.Session() as session:
-        tf.global_variables_initializer().run()
         for epoch in range(training_epochs):
-            print("Epoch {} out of {}".format(epoch + 1, training_epochs))
-            train(train_step, loss_fun, my_model.varname_map, expert_policy, env, session, rtol=args.relative_err)
+            print("Drawing samples, {} of {} epochs".format(epoch + 1, training_epochs))
+            new_obs, new_actions, _ = generate_samples(expert_policy, env, 1000)
+            all_obs.extend(new_obs)
+            all_actions.extend(new_actions)
+        all_obs = np.vstack(tuple(all_obs))
+        all_actions = np.vstack(tuple(all_actions))
+
+    with tf.Session() as session:
+        print(tf.trainable_variables())
+        tf.global_variables_initializer().run()
+        train(train_step, loss_fun, my_model.varname_map, session, all_obs, all_actions, args.batch_size,
+              rtol=args.relative_err)
 
         fname = input("Where do you want to save the derived policy? ")
         with open(fname, 'wb') as savefile:
---
+++ hw1/models/ant.py
@@ -0,0 +1,4 @@
+"""Contains behavior-cloning models for the ant environment"""
+import numpy as np
+import tensorflow as tf
+
